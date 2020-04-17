00074920fb26ab6cff10874f92748ad236bf0a3b
Diogo Guerra <63842251+diogoguerra03@users.noreply.github.com>
2020-04-17 11:15:24 +0100

    Delete Form1.vb

--- Form1.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim l As New Login
-        l.MdiParent = Me
-        l.Show()
-    End Sub
-End Class
